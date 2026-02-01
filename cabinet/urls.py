ad101860843072dcd1869660eb505c067d317479
talo9 <talonin92@mail.ru>
2026-02-02 00:31:30 +0500

    0.0.1.26 - admin

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -11,10 +11,6 @@ urlpatterns = [
     path('finance/', views.finance_view, name='finance'),
     path('api/refresh-stats/', views.refresh_stats, name='refresh_stats'),
     path('api/get-stats/', views.get_stats_json, name='get_stats_json'),
-    # path('api/get-data/', views.get_user_data, name='get_data_json'),
-    # path('api/add-lo/', views.add_user_lo, name='add_user_lo'),
-    # path('api/sub-lo/', views.sub_user_lo, name='sub_user_lo'),
-    # path('api/get-team/', views.get_user_team, name='get_user_team'),
 
     path('api/user/status/', views.get_user_data, name='get_user_data'),
     path('api/user/team/', views.get_user_team, name='get_user_team'),
