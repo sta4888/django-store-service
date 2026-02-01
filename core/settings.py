5656f5f2d72c1692f618991e64cdbea2edff5273
talo9 <talonin92@mail.ru>
2026-02-01 12:18:27 +0500

    0.0.1.5 - add allowed host

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-jv%p)44&7cak2kw&a5e=0^ber53j=_9h6#ptw!j(z16i5tjksy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.130.148.158']
 
 # Application definition
 
