6e7aa0330ba72710c105fbc85eaf803ad9c72d87
Eugene <talonin92@mail.ru>
2026-02-04 08:25:22 +0500

    Add domain names to ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ DEBUG = True
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
-ALLOWED_HOSTS = ["45.130.148.158", "localhost", "45.130.148.146"]
+ALLOWED_HOSTS = ["45.130.148.158", "localhost", "45.130.148.146", "www.everonuz.com", "everonuz.com"]
 
 # Application definition
 
